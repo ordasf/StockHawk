f6200242c983233ab172bd0b260f671c47534805
ordasf <fordas88@gmail.com>
2017-05-15 22:21:00 +0200

    Added more content descriptions

--- app/src/main/java/com/udacity/stockhawk/ui/StockAdapter.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockAdapter.java
@@ -68,7 +68,9 @@ class StockAdapter extends RecyclerView.Adapter<StockAdapter.StockViewHolder> {
 
 
         holder.symbol.setText(cursor.getString(Contract.Quote.POSITION_SYMBOL));
+        holder.symbol.setContentDescription(cursor.getString(Contract.Quote.POSITION_SYMBOL));
         holder.price.setText(dollarFormat.format(cursor.getFloat(Contract.Quote.POSITION_PRICE)));
+        holder.price.setContentDescription(dollarFormat.format(cursor.getFloat(Contract.Quote.POSITION_PRICE)));
 
 
         float rawAbsoluteChange = cursor.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
@@ -86,8 +88,10 @@ class StockAdapter extends RecyclerView.Adapter<StockAdapter.StockViewHolder> {
         if (PrefUtils.getDisplayMode(context)
                 .equals(context.getString(R.string.pref_display_mode_absolute_key))) {
             holder.change.setText(change);
+            holder.change.setContentDescription(change);
         } else {
             holder.change.setText(percentage);
+            holder.change.setContentDescription(percentage);
         }
 
 
