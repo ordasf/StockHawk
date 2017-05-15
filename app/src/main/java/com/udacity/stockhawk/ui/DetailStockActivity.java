640804cb0cbfe1f592ec330c90697f06d9caf5fe
ordasf <fordas88@gmail.com>
2017-05-15 19:50:12 +0200

    Minor changes, fixes, formatting of the numbers and the percentages and setting up the content descriptions

--- app/src/main/java/com/udacity/stockhawk/ui/DetailStockActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailStockActivity.java
@@ -1,6 +1,8 @@
 package com.udacity.stockhawk.ui;
 
 import android.graphics.Color;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
@@ -20,9 +22,12 @@ import com.udacity.stockhawk.model.StockQuote;
 
 import java.io.CharArrayReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import au.com.bytecode.opencsv.CSVReader;
 import butterknife.BindView;
@@ -30,14 +35,36 @@ import butterknife.ButterKnife;
 
 public class DetailStockActivity extends AppCompatActivity {
 
-    @BindView(R.id.tv_detail_stock)
-    TextView detailStockTextView;
+    @BindView(R.id.tv_symbol_detail_stock)
+    TextView symbolTextView;
 
-    @BindView(R.id.tv_history_detail_stock)
-    TextView historyStockTextView;
+    @BindView(R.id.tv_price_detail_stock)
+    TextView priceTextView;
+
+    @BindView(R.id.tv_absolute_change_detail_stock)
+    TextView absoluteChangeTextView;
+
+    @BindView(R.id.tv_percentage_change_detail_stock)
+    TextView percentageChangeTextView;
 
     @BindView(R.id.chart_detail_stock)
     LineChart lineChart;
+
+    private final DecimalFormat dollarFormatWithPlus;
+    private final DecimalFormat dollarFormat;
+    private final DecimalFormat percentageFormat;
+
+    DetailStockActivity() {
+        super();
+
+        dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+        dollarFormatWithPlus.setPositivePrefix("+$");
+        percentageFormat = (DecimalFormat) NumberFormat.getPercentInstance(Locale.getDefault());
+        percentageFormat.setMaximumFractionDigits(2);
+        percentageFormat.setMinimumFractionDigits(2);
+        percentageFormat.setPositivePrefix("+");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,19 +79,24 @@ public class DetailStockActivity extends AppCompatActivity {
             StockQuote quote = extras.getParcelable(MainActivity.DETAIL_STOCK_QUOTE);
             String history = quote.getHistory();
 
-            CSVReader reader = new CSVReader(new CharArrayReader(history.toCharArray()));
-            List<String[]> quotesOverTime = null;
-            try {
-                quotesOverTime = reader.readAll();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            // Populate the data into the Text Views
+            symbolTextView.setText(quote.getSymbol());
+            symbolTextView.setContentDescription(quote.getSymbol());
+
+            priceTextView.setText(dollarFormat.format(quote.getPrice()));
+            priceTextView.setContentDescription(dollarFormat.format(quote.getPrice()));
+
+            absoluteChangeTextView.setText(dollarFormat.format(quote.getAbsoluteChange()));
+            absoluteChangeTextView.setContentDescription(dollarFormat.format(quote.getAbsoluteChange()));
+
+            percentageChangeTextView.setText(percentageFormat.format(quote.getPercentageChange() / 100));
+            percentageChangeTextView.setContentDescription(percentageFormat.format(quote.getPercentageChange() / 100));
+
+            List<String[]> quotesOverTime = readHistoricData(history);
 
             if (quotesOverTime == null) {
                 return;
             }
-
-            Collections.reverse(quotesOverTime);
 
             List<Entry> entries = new ArrayList<>();
             final String[] dateLabels = new String[quotesOverTime.size()];
@@ -97,4 +129,21 @@ public class DetailStockActivity extends AppCompatActivity {
         }
 
     }
+
+    private List<String[]> readHistoricData(String history) {
+
+        CSVReader reader = new CSVReader(new CharArrayReader(history.toCharArray()));
+        List<String[]> quotesOverTime = null;
+        try {
+            quotesOverTime = reader.readAll();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        Collections.reverse(quotesOverTime);
+
+        return quotesOverTime;
+    }
+
 }
