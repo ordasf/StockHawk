e5a3ecaffca076aa8e0333d4493bcf4ab6392c42
ordasf <fordas88@gmail.com>
2017-05-21 00:45:39 +0200

    Firts steps to put the widget in place

---
+++ app/src/main/java/com/udacity/stockhawk/widget/ListWidgetProvider.java
@@ -0,0 +1,6 @@
+package com.udacity.stockhawk.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+public class ListWidgetProvider  extends AppWidgetProvider {
+}
---
+++ app/src/main/java/com/udacity/stockhawk/widget/ListWidgetRemoteViewsServices.java
@@ -0,0 +1,13 @@
+package com.udacity.stockhawk.widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class ListWidgetRemoteViewsServices extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
+
+}
